3169493ed226142e10b20016049d7168ce35866c
YaminiSriNavyaAnisetti <137883714+YaminiSriNavyaAnisetti@users.noreply.github.com>
2023-07-21 18:51:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const button=document.querySelector("button");
+const body=document.querySelector("body");
+const color =['violet','indigo','blue','green','yellow','orange','red'];
+body.style.backgroundColor='pink';
+document.addEventListener('click',function(){
+   const colorIndex=parseInt(Math.random()*color.length);
+   document.getElementById("ans").innerHTML=colorIndex;
+   body.style.backgroundColor=color[colorIndex];
+
+});
